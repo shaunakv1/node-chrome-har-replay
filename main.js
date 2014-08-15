2bc636d027355653c62dfc3bc38463a47f625cb0
shaunakv1 <shaunakv1@gmail.com>
2014-08-15 15:45:03 -0400

    disabled logging.. added function to calculate total time and display it at the end

--- main.js
+++ main.js
@@ -18,33 +18,36 @@ var count = 0;
 var counterCheck = 0;
 
 function done (end) {
-	console.log("**********************************************************************");
-	totalTime = totalTime + end;
-	count = count + 1;
-	
-	if(count >= counterCheck){
-		console.log("total time: "+ totalTime,'s');
-	}
+
 }
 
-async.each(har.log.entries,function(e,done) {
-	if(e.request.url.match('noaa.gov')){
-			counterCheck = counterCheck + 1;
-			var url = urlParser.parse(e.request.url);
-			var start = new Date();
+async.each(har.log.entries.reverse(),function(e,done) {
+    if(e.request.url.match('noaa.gov')){
+            counterCheck = counterCheck + 1;
+            var url = urlParser.parse(e.request.url);
+            var start = new Date();
 
-			http.get({host: url.host,path: url.path, port: 80}, function(res) {
-			    console.log('start : '+ start);
-			    console.log('end   : '+ new Date());
-			    var end = (new Date() - start)/1000.00;
-			    console.log('time  : '+ end , 's');
-			    console.log('-----------------------------------------------');
-			    done(end);
-			});
-		}
+            http.get({host: url.host,path: url.path, port: 80}, function(res) {
+                //console.log('start : '+ start);
+                //console.log('end   : '+ new Date());
+                var end = (new Date() - start)/1000.00;
+                //console.log('time  : '+ end , 's');
+                //console.log('-----------------------------------------------');
+                doneRequest(end);
+            });
+        }
 }, function (err) {
-	console.log('error..');
-	console.log(err);
+    console.log('error..');
+    console.log(err);
 });
+
+function doneRequest(end){
+    totalTime = totalTime + end;
+    count = count + 1;
+
+    if(count >= counterCheck){
+        console.log("total time: "+ totalTime,'s');
+    }
+}
 
 
