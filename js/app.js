eb2c78e996babd8ada131d5c1db8920d165200e2
shaunakv1 <shaunakv1@gmail.com>
2014-08-20 19:49:49 -0400

    added second har to chart

--- js/app.js
+++ js/app.js
@@ -1,67 +1,83 @@
 $(function () {
-        $.getJSON("logs/llv_p_log.json",function (log) {
-            //$.getJSON("avg_log.json",function (avg) {
-                var avg = [];
-                /*avg.forEach(function (v) {
-                    v[0] = Date.parse(v[0]);
-                });*/
+        $.when(
+            $.getJSON('logs/llv_p_log.json'),
+            $.getJSON('logs/llv_p_avg_log.json'),
+            $.getJSON('logs/slr_p_log.json'),
+            $.getJSON('logs/slr_p_avg_log.json')
+        ).then(function(log, avg, slrLog, slrAvg) {
+              log = log[0];
+              avg = avg[0];
 
-                log.forEach(function (v) {
-                    $('#table').append('<div><span class="date">'+new Date(Date.parse(v[0]))+'</span> <span class="resTime">'+parseInt(v[1])+'sec</span></div>')
-                    v[0] = Date.parse(v[0]);
-                    avg.push([v[0], (v[1]/250)]);
-                });
-                console.log(avg);
-                $('#chart').highcharts({
-                    chart: {
-                        type: 'spline',
-                        zoomType: 'x'
-                    },
-                    plotOptions: {
-                        series: {
-                            marker: {
-                                enabled: false
-                            }
+              slrLog = slrLog[0];
+              slrAvg = _.map(slrAvg[0],function(v){ return [v[0],v[1]+200]; }); // offset slr average by 200 so it is visible
+
+               [log, avg, slrLog, slrAvg].forEach(function(arr){
+                    arr.forEach(function (v) {
+                        v[0] = Date.parse(v[0]);
+                    });
+               });
+
+               $('#chart').highcharts({
+                   chart: {
+                       type: 'spline',
+                       zoomType: 'x'
+                   },
+                   plotOptions: {
+                       series: {
+                           marker: {
+                               enabled: false
+                           },
+                           states: {
+                                   hover: {
+                                       enabled: false,
+                                       lineWidth: null
+                                   }
+                               }
+                       }
+                   },
+                   title: {
+                       text: 'LLV Map Services Network Performance'
+                   },
+                   subtitle: {
+                       text: 'ArcGIS Cache services'
+                   },
+                   xAxis: {
+                       type: 'datetime',
+                       tickInterval: 1000 * 60 * 60 * 6,
+
+                      min: log[0][0],
+                      max: log[log.length -1 ][0],
+                      dateTimeLabelFormats : {
+                              hour: '%I %p',
+                              minute: '%I:%M %p'
                         }
-                    },
-                    title: {
-                        text: 'LLV Map Services Network Performance'
-                    },
-                    subtitle: {
-                        text: 'ArcGIS Cache services'
-                    },
-                    xAxis: {
-                        type: 'datetime',
-                        tickInterval: 1000 * 60 * 60 * 6,
+                   },
+                   yAxis: {
+                       title: {
+                           text: 'Total Response Time (secs)'
+                       },
+                       min: 0,
+                       max: 3000
 
-                       min: log[0][0],
-                       max: log[log.length -1 ][0],
-                       dateTimeLabelFormats : {
-                               hour: '%I %p',
-                               minute: '%I:%M %p'
-                         }
-                    },
-                    yAxis: {
-                        title: {
-                            text: 'Total Response Time (secs)'
-                        },
-                        min: 0,
-                        max: 2000
+                   },
+                   /*tooltip: {
+                       headerFormat: '<b>{series.name}</b><br>',
+                       pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
+                   },*/
 
-                    },
-                    /*tooltip: {
-                        headerFormat: '<b>{series.name}</b><br>',
-                        pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
-                    },*/
-
-                    series: [{
-                        name: 'Total Time',
-                        data: log
-                    },{
-                        name: 'Average Time',
-                        data: avg
-                    }]
-                });
-            //});
+                   series: [{
+                       name: 'LLV Total Time',
+                       data: log
+                   },{
+                       name: 'LLV Average Time',
+                       data: avg
+                   },{
+                       name: 'SLR Total Time',
+                       data: slrLog
+                   },{
+                       name: 'SLR Average Time',
+                       data: slrAvg
+                   }]
+               });
         });
 });
