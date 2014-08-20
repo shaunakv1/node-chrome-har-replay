1f3f563836f1f0f080d3032d06c94a4e480cdb31
shaunakv1 <shaunakv1@gmail.com>
2014-08-20 18:54:38 -0400

    refactored and enhanced to be able to capture more than one har files and save more than one logs

--- main.js
+++ main.js
@@ -1,33 +1,41 @@
-var har = require('./llv_har2.json');
-var http = require('http');
+var request = require('request');
+var moment = require('moment');
 var fs = require('fs');
 var async = require('async');
 var urlParser = require('url');
 
-var copyLogsTo = "L:/httpd/apps/ads/llv-network-performance/"; //mark as null if copying is not needed
-var logAvg = false;
+var copyLogsTo = null;//"L:/httpd/apps/ads/llv-network-performance/"; //mark as null if copying is not needed
 
-function PerformanceTest(){
+function PerformanceTest(options){
+    this.har = require('./har/'+ options.har);
+    this.logFile = './logs/'+ options.log;
+    this.avgLogFile = './logs/'+ options.avgLog;
+    this.label =   options.label;
+    this.stdOut = options.stdOut;
+
     this.totalTime = 0;
     this.count = 0;
     this.counterCheck = 0;
     this.totalRequests = 0;
+    this.successRequests = 0;
+    this.startTime = "";
     var THIS = this;
     function runTest(){
-        process.stdout.write('running test at:'+ new Date());
-        async.each(har.log.entries.reverse(),function(e) {
+        THIS.startTime = moment().format('MMM Do YY, h:mm:ss a');
+        async.each(THIS.har.log.entries.reverse(),function(e) {
             if(e.request.url.match('noaa.gov')){
                     THIS.counterCheck = THIS.counterCheck + 1;
                     THIS.totalRequests = THIS.totalRequests + 1;
-                    var url = urlParser.parse(e.request.url);
+                    var url = e.request.url;//urlParser.parse(e.request.url);
                     var start = new Date();
 
-                    http.get({host: url.host,path: url.path, port: 80}, function(res) {
+                    request(url, function(error,res) {
+                        if(res.statusCode == 200)  THIS.successRequests =  THIS.successRequests + 1;
                         /*console.log('start : '+ start);
                         console.log('end   : '+ new Date());*/
                         var end = (new Date() - start)/1000.00;
-                        /*console.log('time  : '+ end , 's');
-                        console.log('-----------------------------------------------');*/
+                        //console.log('time  : '+ end , 's');
+                        //console.log('-----------------------------------------------');
                         THIS.doneRequest(end);
                     });
                 }
@@ -45,11 +53,13 @@ PerformanceTest.prototype.doneRequest = function (end){
     THIS.totalTime = THIS.totalTime + end;
     THIS.count = THIS.count + 1;
     if(THIS.count >= THIS.counterCheck){
-        console.log("...done...total time: "+ THIS.totalTime+"s");
-        var log = require('./log.json');
+        if(THIS.stdOut) process.stdout.write("\n"+THIS.label +' :---> '+ THIS.startTime +' :---> ');
+        if(THIS.stdOut)  console.log(" time: "+ THIS.totalTime+"s  (req: "+THIS.successRequests+"/"+THIS.totalRequests+")");
+
+        var log = require(THIS.logFile);
         var time = new Date();
         log.push([time, THIS.totalTime]);
-        fs.writeFile("./log.json", JSON.stringify(log), function(err) {
+        fs.writeFile(THIS.logFile, JSON.stringify(log), function(err) {
                 if(err) {
                     console.log(err);
                 }
@@ -57,27 +67,56 @@ PerformanceTest.prototype.doneRequest = function (end){
                     THIS.totalTime = 0;
                     THIS.count = 0;
                     THIS.counterCheck = 0;
-                    if(copyLogsTo) fs.createReadStream('./log.json').pipe(fs.createWriteStream(copyLogsTo+'log.json'));
+                    THIS.successRequests =0;
+                    if(copyLogsTo) fs.createReadStream(THIS.logFile).pipe(fs.createWriteStream(copyLogsTo+'log.json'));
                 }
             });
 
-        if(logAvg){
-            var log = require('./avg_log.json');
+            var log = require(THIS.avgLogFile);
             log.push([time, THIS.totalTime/THIS.totalRequests]);
-            fs.writeFile("./avg_log.json", JSON.stringify(log), function(err) {
+            fs.writeFile(THIS.avgLogFile, JSON.stringify(log), function(err) {
                     if(err) {
                         console.log(err);
                     }
                     else{
                         THIS.totalRequests = 0;
-                        if(copyLogsTo) fs.createReadStream('./avg_log.json').pipe(fs.createWriteStream(copyLogsTo+'avg_log.json'));
+                        if(copyLogsTo) fs.createReadStream(THIS.avgLogFile).pipe(fs.createWriteStream(copyLogsTo+'avg_log.json'));
                     }
                 });
         }
-    }
 
 };
 
-new PerformanceTest();
-setInterval(function() { new PerformanceTest(); }, 1000 * 60 * 30 );
+/**
+ * Run for LLV Production
+ */
 
+var opt_llv_p = {
+    har:'llv_p_har.json',
+    log:'llv_p_log.json',
+    avgLog:'llv_p_avg_log.json',
+    stdOut:true,
+    label:"LLV PR"
+};
+
+var opt_llv_qa = {
+    har:'llv_qa_har.json',
+    log:'llv_qa_log.json',
+    avgLog:'llv_qa_avg_log.json',
+    stdOut:true,
+    label:"LLV QA"
+};
+
+
+
+//Run LLV Production
+new PerformanceTest(opt_llv_p);
+setInterval(function() { new PerformanceTest(opt_llv_p); }, 1000 * 60 * 30 );
+
+
+//Run LLV Webqa
+// 10 minutes after runing last sequence, start running qa
+setTimeout(function() {
+      new PerformanceTest(opt_llv_qa);
+      setInterval(function() { new PerformanceTest(opt_llv_qa); }, 1000 * 60 * 30 );
+}, 1000 * 60 * 10);
